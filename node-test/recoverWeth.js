15434939f72dfbf8067ef450649de5a00ec8a703
henry <henry@orangegrin.com>
2023-07-17 08:30:47 +0800

    fix recover weth

--- node-test/recoverWeth.js
+++ node-test/recoverWeth.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 const ethers = require('ethers');
+const wethContractABI = require('./abi/IWETH.json');
 // Create an ethers provider instance
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.DEPOLY_KEY, provider);
-
+const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 async function main() {
-    //destory 
+    // 使用合约地址和 ABI 创建 WETH 合约实例
+    const wethContract = new ethers.Contract(wethAddress, wethContractABI, wallet);
+    const wethBalance = await wethContract.balanceOf(process.env.SANDWICH_CONTRACT);
     const nonce = await wallet.getTransactionCount();
     const gasPrice = await provider.getGasPrice();
     const gasLimit = 500000; // 根据具体情况设置适当的 gas limit
-
+    
+    // Craft our payload
+    const payload = ethers.utils.solidityPack(
+        ["uint8", "uint256"],
+        [
+            66,
+            wethBalance
+        ]
+    );
+    console.log(payload);
     const unsignedTx = {
         to: process.env.SANDWICH_CONTRACT,
-        data: '0x42',
+        data: payload,
         nonce,
         gasPrice,
         gasLimit
