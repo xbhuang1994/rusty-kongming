b695f648407ef31b1903849547c34d14320a22cf
henry <henry@orangegrin.com>
2023-08-10 12:46:41 +0200

    append multi call

--- bot/crates/strategy/src/tx_utils/huff_sando_interface/common/mod.rs
+++ bot/crates/strategy/src/tx_utils/huff_sando_interface/common/mod.rs
@@ -7,7 +7,7 @@ pub mod five_byte_encoder;
 pub mod weth_encoder;
 
 // Declare the array as static
-static FUNCTION_NAMES: [&str; 8] = [
+static FUNCTION_NAMES: [&str; 18] = [
     "v2_backrun0",
     "v2_frontrun0",
     "v2_backrun1",
@@ -16,6 +16,16 @@ static FUNCTION_NAMES: [&str; 8] = [
     "v3_frontrun0",
     "v3_backrun1",
     "v3_frontrun1",
+    "seppuku",
+    "recoverEth",
+    "recoverWeth",
+    "v2_backrun_multi",
+    "v2_frontrun_multi",
+    "v3_backrun0_multi",
+    "v3_frontrun0_multi",
+    "v3_backrun1_multi",
+    "v3_frontrun1_multi",
+    "check_block_number"
 ];
 
 pub fn get_jump_dest_from_sig(function_name: &str) -> u8 {
