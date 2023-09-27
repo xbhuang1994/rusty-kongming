2d8313bfc8e106b0d369a7282db35f93246df6a8
dictwang <dictwang@163.com>
2023-09-27 17:52:20 +0800

    add debug log

--- bot/crates/strategy/src/types.rs
+++ bot/crates/strategy/src/types.rs
@@ -293,6 +293,7 @@ impl SandoRecipe {
             .revenue
             .checked_sub(U256::from(self.frontrun_gas_used) * self.target_block.base_fee_per_gas)
             .ok_or_else(|| {
+                println!("revenue={:?}, frontrun_gas_used={:?}, base_fee_per_gas={:?}", self.revenue, self.frontrun_gas_used, self.target_block.base_fee_per_gas);
                 anyhow!("[FAILED TO CREATE BUNDLE] revenue doesn't cover frontrun basefee")
             })?;
 
@@ -346,6 +347,8 @@ impl SandoRecipe {
             .set_simulation_block(self.target_block.number - 1)
             .set_simulation_timestamp(self.target_block.timestamp.as_u64());
 
+        
+        println!("find effective meet");
         Ok(bundle_request)
     }
 }
