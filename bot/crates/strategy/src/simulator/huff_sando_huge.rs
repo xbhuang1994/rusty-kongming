06a981ee6255c1127a39c3deb7548d0f55301041
dictwang <dictwang@163.com>
2023-10-02 22:43:54 +0800

    modify gas_limit

--- bot/crates/strategy/src/simulator/huff_sando_huge.rs
+++ bot/crates/strategy/src/simulator/huff_sando_huge.rs
@@ -38,7 +38,6 @@ pub fn create_recipe_huge(
     
     #[allow(unused_mut)]
     let mut fork_db = CacheDB::new(shared_backend);
-    let meats_count = meats.len() as u64;
 
     #[cfg(feature = "debug")]
     {
@@ -93,7 +92,7 @@ pub fn create_recipe_huge(
     // setup evm for frontrun transaction
     let mut frontrun_tx_env = TxEnv {
         caller: searcher.0.into(),
-        gas_limit: 700000 * meats_count,
+        gas_limit: 700000,
         gas_price: next_block.base_fee_per_gas.into(),
         gas_priority_fee: None,
         transact_to: TransactTo::Call(sando_address.0.into()),
@@ -195,7 +194,7 @@ pub fn create_recipe_huge(
     // setup evm for backrun transaction
     let mut backrun_tx_env = TxEnv {
         caller: searcher.0.into(),
-        gas_limit: 700000 * meats_count,
+        gas_limit: 700000,
         gas_price: next_block.base_fee_per_gas.into(),
         gas_priority_fee: None,
         transact_to: TransactTo::Call(sando_address.0.into()),
