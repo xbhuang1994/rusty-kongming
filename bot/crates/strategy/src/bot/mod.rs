b4f5be9569b47930b27f769c1654ce9afe7bc376
dictwang <dictwang@163.com>
2023-11-16 17:43:07 +0800

    try fixed bug

--- bot/crates/strategy/src/bot/mod.rs
+++ bot/crates/strategy/src/bot/mod.rs
@@ -10,9 +10,7 @@ use ethers::{
 };
 use foundry_evm::executor::fork::{BlockchainDb, BlockchainDbMeta, SharedBackend};
 use foundry_evm::revm::primitives::{B160, U256 as rU256};
-use foundry_evm::revm::new;
 use log::{error, info};
-use tokio::task::JoinError;
 use std::{collections::{BTreeSet, LinkedList, HashMap}, sync::{Arc,Mutex}, time, thread};
 use tokio::{runtime, sync::broadcast::Sender};
 
@@ -87,7 +85,7 @@ impl<M: Middleware + 'static> SandoBot<M> {
                 config.sando_inception_block,
             ),
             sando_recipe_manager: SandoRecipeManager::new(),
-            event_tx_runtime: if need_runtime {Some(runtime::Builder::new_multi_thread().worker_threads(32).enable_all().enable_time().build().unwrap())} else {None},
+            event_tx_runtime: if need_runtime {Some(runtime::Builder::new_multi_thread().worker_threads(32).enable_all().build().unwrap())} else {None},
             event_tx_list: Arc::new(Mutex::new(LinkedList::new())),
             event_tx_sender: Arc::new(Mutex::new(None)),
             event_block_runtime: if need_runtime {Some(runtime::Builder::new_multi_thread().worker_threads(4).enable_all().build().unwrap())} else {None},
@@ -976,8 +974,10 @@ impl<M: Middleware + 'static> SandoBot<M> {
                         loop {
                             match self.pop_event_tx().await {
                                 Some(event) => {
-                                    match self.process_event_tx(event).await {
-                                        Ok(_) => {},
+                                    match self.process_event_tx(event, _index).await {
+                                        Ok(_) => {
+                                            continue;
+                                        },
                                         Err(e) => error!("bot running event tx processor {_index} error {}", e),
                                     }
                                 },
@@ -1344,9 +1344,9 @@ impl<M: Middleware + 'static> SandoBot<M> {
     }
 
     /// Process incoming events of transaction
-    async fn process_event_tx(&self, event: Transaction) -> Result<()> {
+    async fn process_event_tx(&self, event: Transaction, thread_number: i32) -> Result<()> {
         // info!("proc tx {:?}", event.hash);
-        if let Some(action) = self.process_new_tx(event).await {
+        if let Some(action) = self.process_new_tx(event, thread_number).await {
            self.push_action(action).await?;
         }
 
@@ -1446,12 +1446,15 @@ impl<M: Middleware + 'static> SandoBot<M> {
     }
 
     async fn pop_event_tx(&self) -> Option<Transaction> {
-        let mut list_tx = self.event_tx_list.lock().unwrap();
-        if !list_tx.is_empty() {
-            list_tx.pop_front()
-        } else {
-            None
+        match self.event_tx_list.try_lock() {
+            Ok(mut list_tx) => {
+                if !list_tx.is_empty() {
+                    return list_tx.pop_front();
+                }
+            },
+            Err(_) => {}
         }
+        None
     }
 
     async fn pop_event_block(&self) -> Option<NewBlock> {
@@ -1590,7 +1593,7 @@ impl<M: Middleware + 'static> SandoBot<M> {
 
     /// Process new txs as they come in
     #[allow(unused_mut)]
-    async fn process_new_tx(& self, victim_tx: Transaction) -> Option<Action> {
+    async fn process_new_tx(& self, victim_tx: Transaction, thread_number: i32) -> Option<Action> {
         // setup variables for processing tx
         let next_block = self.block_manager.get_next_block();
         let latest_block = self.block_manager.get_latest_block();
@@ -1640,7 +1643,7 @@ impl<M: Middleware + 'static> SandoBot<M> {
         if !touched_pools.is_empty() {
             for pool in touched_pools {
                 let (head_hashs, head_txs) = self.sando_state_manager.get_head_txs(&victim_tx.from, pool.address(), SandwichSwapType::Forward);
-                log_info_cyan!("process sandwich {:?} from {:?} nonce {:?} pool {:?} head_txs {:?}", victim_tx.hash, victim_tx.from, victim_tx.nonce, pool.address(), head_hashs.join(","));
+                log_info_cyan!("thread-{:?} process sandwich {:?} from {:?} nonce {:?} pool {:?} head_txs {:?}", thread_number, victim_tx.hash, victim_tx.from, victim_tx.nonce, pool.address(), head_hashs.join(","));
                 
                 let (token_a, token_b) = match pool {
                     UniswapV2(p) => (p.token_a, p.token_b),
@@ -1733,7 +1736,7 @@ impl<M: Middleware + 'static> SandoBot<M> {
         if !touched_pools_reverse.is_empty() {
             for pool in touched_pools_reverse {
                 let (head_hashs, head_txs) = self.sando_state_manager.get_head_txs(&victim_tx.from, pool.address(), SandwichSwapType::Reverse);
-                log_info_cyan!("process reverse sandwich {:?} from {:?} nonce {:?} pool {:?} head_txs {:?}", victim_tx.hash, victim_tx.from, victim_tx.nonce, pool.address(), head_hashs.join(","));
+                log_info_cyan!("thread-{:?} process reverse sandwich {:?} from {:?} nonce {:?} pool {:?} head_txs {:?}", thread_number, victim_tx.hash, victim_tx.from, victim_tx.nonce, pool.address(), head_hashs.join(","));
                 
                 let (token_a, token_b) = match pool {
                     UniswapV2(p) => (p.token_a, p.token_b),
